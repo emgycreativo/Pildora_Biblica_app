7f819658cdc5c02ef74470b539b96809666ed06f
emgycreativo <em.gy.melvin@gmail.com>
2025-06-25 18:27:27 -0500

    Add slide transition overlay and update plus button style

--- lib/cita_confirmada.dart
+++ lib/cita_confirmada.dart
@@ -76,7 +76,7 @@ class _CitaConfirmadaState extends State<CitaConfirmada> {
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
               decoration: BoxDecoration(
-                color: Colors.red.shade200,
+                color: Colors.black,
                 borderRadius: BorderRadius.circular(12),
               ),
               child: const Text('PLUS', style: TextStyle(color: Colors.white)),
